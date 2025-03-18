63e645deb96393846001b7d123a3962b1af6998e
AdemHos <ademhos1907@outlook.com>
2025-03-18 19:56:35 +0300

    Course Section and Courses Card has created

--- src/components/Student/CoursesSection.jsx
+++ src/components/Student/CoursesSection.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from '../Student/CourseCard'
 
 const CoursesSection = () => {
+
+  const {allCourses} = useContext(AppContext)
+
   return (
     <div>
-      <h1>Courses Section</h1>
+      <h2 className='text-gray-800 font-medium text-3xl'>Best Tutors are here</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Explore online world,improve yourself,gain new skillsets and take to your career next level</p>
+
+      <div className='grid grid-cols-auto gap-4 px-4 md:px-0 my-10 md:my-16'>
+        {allCourses.slice(0,4).map((course,index) => <CourseCard key={index} course={course}/> ) }
+      </div>
+      
+      <Link to={'/course-list'} onClick={() => scrollTo(0,0)} className=
+      'text-gray-500 border border-gray-500 px-10 py-3 rounded-lg'>
+      Show all Courses
+      </Link>
     </div>
   )
 }
--- src/pages/Student/Home.jsx
+++ src/pages/Student/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Student/Hero'
 import Conpanies from '../../components/Student/Conpanies'
+import CoursesSection from '../../components/Student/CoursesSection'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
       <Conpanies/>
+      <CoursesSection/>
     </div>
   )
 }
