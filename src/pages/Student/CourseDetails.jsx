6d64bb0608e813c5ec779de01f9a998a3d278327
AdemHos <ademhos1907@outlook.com>
2025-03-25 17:14:18 +0300

    Course  Details page has complete

--- src/pages/Student/CourseDetails.jsx
+++ src/pages/Student/CourseDetails.jsx
@@ -4,13 +4,19 @@ import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/Student/Loading'
 import { assets } from '../../assets/assets'
 import humanizeduration from 'humanize-duration'
+import Footer from '../../components/Student/Footer'
+import YouTube from 'react-youtube'
 
 
 const CourseDetails = () => {
   const {id} = useParams()
   const [courseData,setCourseData] = useState(null)
+  const [isAlreadyEnrolled,setIsAlreadyEnrolled] = useState(false)
   const [openSections,setOpenSections] = useState({})
-  const {allCourses,calculateRating,calculateNoOfLectures,calculateChapterTime,calculateCourseDuration} = useContext(AppContext)
+  const [playerData,setPlayerData] = useState(null)
+
+
+  const {allCourses,calculateRating,calculateNoOfLectures,calculateChapterTime,calculateCourseDuration,currency} = useContext(AppContext)
   const fetchCourseData = async  () => {
    const findCourse = allCourses.find(course => course._id === id)
    setCourseData(findCourse)
@@ -66,7 +72,8 @@ const CourseDetails = () => {
                       <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
                         <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
                           <div onClick={() => toggleSection(index)} className='flex items-center gap-2'>
-                            <img src={assets.down_arrow_icon} alt="" />
+                            <img className={`transform transition-transform 
+                              ${openSections[index] ? 'rotate-180' : ''}`} src={assets.down_arrow_icon} alt="" />
                             <p className='md:text-base text-sm font-medium'>{chapter.chapterTitle}</p>
                           </div>
                           <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
@@ -83,7 +90,9 @@ const CourseDetails = () => {
                                 text-xs md:text-default'>
                                   <p>{lecture.lectureTitle}</p>
                                   <div className='flex gap-2'>
-                                    {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                                    {lecture.isPreviewFree && <p onClick={() => setPlayerData({
+                                      videoId: lecture.lectureUrl.split('/').pop()
+                                    })} className='text-blue-500 cursor-pointer'>Preview</p>}
                                     <p>{humanizeduration(lecture.lectureDuration * 60 * 1000,
                                       {units: ['h','m']}
                                     )}</p>
@@ -97,11 +106,74 @@ const CourseDetails = () => {
                     ))}
                   </div>
                 </div>
+                <div className='py-20 text-sm md:text-default'>
+                 <h3 className='text-xl font-semibold text-gray-800'>Course Description</h3>
+                 <p className='pt-3 rich-text'
+        dangerouslySetInnerHTML={{__html:courseData.courseDescription}}></p>
+                </div>
         
-      </div>
+              </div>
       {/* Right Column */}
-      <div></div>
+      <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none
+      overflow-hidden bg-white min-w[300px] sm:min-w-[420px]'>
+        {
+          playerData ?
+          <YouTube videoId={playerData.videoId} options={{playerVars: {
+            autoPlay: 1
+          }}} iframeClassName='w-full aspect-video'/>
+          :  <img src={courseData.courseThumbnail} alt="" />
+        }
+        <div className='p-5'>
+          <div className='flex items-center gap-2'>
+            <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+            <p className='text-red-500'> <span className='font-medium'> 5 days</span> left at this price!!</p>
+          </div>
+          <div className='flex items-center gap-3 pt-2'>
+            <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>{currency} {(courseData.coursePrice - courseData.discount * courseData.coursePrice/100).toFixed(2)}</p>
+            <p className='md:text-lg text-gray-500 line-through'>{currency} {courseData.coursePrice}</p>
+            <p className='md:text-lg text-green-600'>{courseData.discount}% off</p>
+          </div>
+            {/* Details */}
+          <div className='flex items-center gap-4 pt-2 md:pt-4 text-gray-500 text-sm md:text-default'>
+              <div className='flex items-center gap-1'>
+                <img src={assets.star} alt="star icon" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+
+              <div className='h-4 w-px bg-gray-500'></div>
+
+              <div className='flex items-center gap-1'>
+                <img src={assets.time_clock_icon} alt="time clock icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className='h-4 w-px bg-gray-500'></div>
+
+              <div className='flex items-center gap-1'>
+                <img src={assets.lesson_icon} alt="lesson icon" />
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+              
+          </div>
+          <button className='md:mt-6 mt-4 bg-black text-white w-full py-3 rounded-md font-medium'>{isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
+
+          <div className='pt-6'>
+            <p className='md:text-xl text-lg font-medium text-gray-800 '>What's in the course</p>
+            <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>
+              <li>Lifetime Access with Free Updates</li>
+              <li>Project base guidance</li>
+              <li>Downloadable resources and source code</li>
+              <li>Exams for test your knowledge</li>
+              <li>Complation Certificate</li>
+
+            </ul>
+          </div>
+
+
+        </div>
+      </div>
     </div>
+    <Footer/>
     </>
     
   ): <Loading/>
